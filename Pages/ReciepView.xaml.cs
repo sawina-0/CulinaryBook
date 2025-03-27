51fc713f7a02eaf4305057255e78b826d20bfe18
sawina_ <olga.fedorovar@gmail.com>
2025-03-27 14:08:49 +0300

    with button i guess

--- Pages/ReciepView.xaml.cs
+++ Pages/ReciepView.xaml.cs
@@ -21,13 +21,14 @@ namespace CulinaryBook.Pages
     /// </summary>
     public partial class ReciepView : Page
     {
-        //private List<Recipes> allRecipes;
+        private List<Recipes> allRecipes;
         public ReciepView()
         {
             InitializeComponent();
             //ComboFilter.ItemsSource = AppConnect.Model.Categories;
-            ListProducts.ItemsSource = AppConnect.Model.Recipes.ToList();
-            //allRecipes = AppConnect.Model.Recipes.ToList();
+            //ListProducts.ItemsSource = AppConnect.Model.Recipes.ToList();
+            allRecipes = AppConnect.Model.Recipes.ToList();
+            ListProducts.ItemsSource = allRecipes;
 
         }
 
@@ -38,13 +39,31 @@ namespace CulinaryBook.Pages
 
         private void ComboSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if(ComboSort.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string sortBy = selectedItem.Content.ToString();
+                List<Recipes> sortedResipes;
+                switch(sortBy)
+                {
+                    case "Sort by name":
+                        sortedResipes = allRecipes.OrderBy(recipe => recipe.RecipeName).ToList();
+                        break;
+                    case "Sort by time":
+                        sortedResipes = allRecipes.OrderBy(recipe => recipe.CookingTime).ToList();
+                        break;
+                    default:
+                        sortedResipes = allRecipes;
+                        break;
+                }
+                ListProducts.ItemsSource = sortedResipes;
+            }
         }
 
         private void TextSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            //string searchText = TextSearch.Text.ToLower();
-            //var filteredRecipes = allRecipes.Where()
+            string searchText = TextSearch.Text.ToLower();
+            var filteredRecipes = allRecipes.Where(recipe => recipe.RecipeName.ToLower().Contains(searchText)).ToList();
+            ListProducts.ItemsSource = filteredRecipes;
         }
 
         private void btChangeRecipe_Click(object sender, RoutedEventArgs e)
@@ -62,7 +81,7 @@ namespace CulinaryBook.Pages
 
         private void btAdd_Click(object sender, RoutedEventArgs e)
         {
-
+            NavigationService.Navigate(new AddRecipe());
         }
     }
 }
